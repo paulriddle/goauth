ee6198a4f135123e1a6d61d7ad44479b850168a5
Paul Riddle <fixnum42@gmail.com>
2020-09-19 18:52:53 +0500

    Add buttons and redirect endpoint

--- client/main.go
+++ client/main.go
@@ -5,20 +5,53 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 )
+
+const (
+	serverAddress     = "http://localhost:8080"
+	authServerAddress = "http://localhost:8081"
+	protectedResource = "http://localhost:8082"
+)
+
+type clientInfo struct {
+	id            string
+	secret        string
+	scope         string
+	redirect_uris []string
+}
+
+type authServerInfo struct {
+	authEndpoint  string
+	tokenEndpoint string
+}
 
 type credentials struct {
 	accessToken string
 }
 
 type serverError struct {
-	StatusCode int
-	Message    string
+	StatusCode    int
+	StatusMessage string
+	ErrorMessage  string
 }
 
 func main() {
+	// Panic if any of the templates are missing
 	indexTempl := newTemplate("index.html")
 	errorTempl := newTemplate("error.html")
+
+	client := clientInfo{
+		id:            "goauth",
+		secret:        "random-string",
+		scope:         "all",
+		redirect_uris: []string{serverAddress + "/callback"},
+	}
+
+	authServer := authServerInfo{
+		authEndpoint:  authServerAddress + "/authorize",
+		tokenEndpoint: authServerAddress + "/token",
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -26,8 +59,18 @@ func main() {
 		indexTempl.Execute(w, creds)
 	})
 
-	mux.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
-	})
+	authorizeURL, err := url.Parse(authServer.authEndpoint)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	authorizeURL.Query().Set("response_type", "code")
+	authorizeURL.Query().Set("scope", client.scope)
+	authorizeURL.Query().Set("client_id", client.id)
+	authorizeURL.Query().Set("redirect_uri", client.redirect_uris[0])
+
+	mux.Handle("/authorize",
+		http.RedirectHandler(authorizeURL.String(), http.StatusFound))
+
 	mux.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 	})
 
@@ -36,17 +79,19 @@ func main() {
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
 			whatHappened := serverError{
-				StatusCode: http.StatusUnauthorized,
-				Message:    http.StatusText(http.StatusUnauthorized),
+				StatusCode:    http.StatusUnauthorized,
+				StatusMessage: http.StatusText(http.StatusUnauthorized),
+				ErrorMessage:  "Missing access token",
 			}
 			errorTempl.Execute(w, whatHappened)
 			return
 		}
+		// http.Post(protectedResource, )
 		creds := credentials{accessToken: accessToken.Name}
 		indexTempl.Execute(w, creds)
 	})
 
-	fmt.Println("Listening on http://localhost:8080")
+	fmt.Println("Listening on " + serverAddress)
 	http.ListenAndServe(":8080", mux)
 }
 
